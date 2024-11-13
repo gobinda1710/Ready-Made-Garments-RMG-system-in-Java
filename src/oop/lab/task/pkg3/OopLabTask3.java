8283cf13fdcc6fb09b5cbbfe7d652c8121445dae
gobinda1710 <das23105101193@diu.edu.bd>
2024-11-13 13:17:57 +0600

    Add new java

--- src/oop/lab/task/pkg3/OopLabTask3.java
+++ src/oop/lab/task/pkg3/OopLabTask3.java
@@ -24,6 +24,34 @@ class Garment {
         return discount;
     }
 }
+class Fabric {
+
+    public String id;
+    public String type;
+    public String color;
+    public double pricePerMeter;
+
+    double calculateCost(double meters) {
+        double newPrice = pricePerMeter * meters;
+        return newPrice;
+    }
+}
+class Supplier {
+
+    public String id;
+    public String name;
+    public String contactInfo;
+    //List
+    List<Fabric> suppliedFabric = new ArrayList<>();
+
+    void addFabric(Fabric fabric) {
+        suppliedFabric.add(fabric);
+    }
+
+    List<Fabric> getSuppliedFabrics() {
+        return suppliedFabric;
+    }
+}
 
 
 public class OopLabTask3 {
